e186cd94f37e064d666347940abe6cf77995716b
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-02-16 16:49:02 +0100

    rendu

--- src/components/Liste.vue
+++ src/components/Liste.vue
@@ -1,6 +1,25 @@
 <template>
   <div >
-    <h2>Détails : {{ $route.params.id }}</h2>
+    <h2>ID : {{ $route.params.id }}</h2>
+
+   <h2>Titre : {{ post.Title }}</h2>
+  <p> Année de sortie : {{ post.Year }}</p>
+ <p>Age conseillé : {{ post.Rated }}</p>
+  <p>Date de réalisation : {{ post.Realeased }}</p>
+  <p>Durée du film : {{ post.Runtime }}</p>
+   <p>Genre :{{ post.Genre }}</p>
+   <p>Directeur :{{ post.Director }}</p>
+   <p>Ecriture :{{ post.Writer }}</p>
+  <p>Acteurs : {{ post.Actors }}</p>
+  <p>Scenario : {{ post.Plot }}</p>
+  <p>Pays : {{ post.Country }}</p>
+  <p>Prix : {{ post.Awards }}</p>
+  <p>Notes : {{ post.Ratings }}</p>
+  <p>Note Metascore : {{ post.Metascore }}</p>
+  <p>Date de sortie DVD : {{ post.DVD }}</p>
+  <p>Type : {{ post.Type }}</p>
+  <p>BoxOffice : {{ post.BoxOffice }}</p>
+  <p>Production : {{ post.Production }}</p>
 
   </div>
 </template>
@@ -13,13 +32,16 @@ export default {
 
      data() {
         return {
-            info: null,
+            post : null
         }
     },
     mounted () {
         axios
             .get('http://www.omdbapi.com/?i='+this.$route.params.id+'&apikey=904eed5c')
-            .then(response => (this.info = response.data.Search ))
+            .then((result) => {
+                                 this.post = result.data;
+                                  console.log(this.post);
+                                })
     },
 }
 
